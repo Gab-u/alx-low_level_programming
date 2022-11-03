cf86d3a28fc232d93bdfd13be755c38e882ee26c
Gab-u <gabrielsakwa@gmail.com>
2022-11-03 11:44:54 -0700

    Advanced task 100 on recursion

---
+++ 0x08-recursion/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = is_palindrome("level");
+	printf("%d\n", r);
+	r = is_palindrome("test");
+	printf("%d\n", r);
+	return (0);
+}
