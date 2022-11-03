d2a7610253aa078e937d92672036975d6f88c542
Gab-u <gabrielsakwa@gmail.com>
2022-11-03 11:06:17 -0700

    Task 6 on recursion

---
+++ 0x08-recursion/6-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = is_prime_number(1);
+	printf("%d\n", r);
+	r = is_prime_number(17);
+	printf("%d\n", r);
+	r = is_prime_number(-1);
+	printf("%d\n", r);
+	r = is_prime_number(113);
+	printf("%d\n", r);
+	return (0);
+}
