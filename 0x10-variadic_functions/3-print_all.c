71a8e4679c5afd2013e5c01b119187bba20ebf34
Gab-u <gabrielsakwa@gmail.com>
2022-11-19 04:12:06 -0800

    correction task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,6 @@
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
- * Return: void
  */
 void print_all(const char * const format, ...)
 {
@@ -22,15 +21,12 @@ void print_all(const char * const format, ...)
 			{
 				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
-
 					break;
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
-
 					break;
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
-
 					break;
 				case 's':
 					str = va_arg(list, char *);
